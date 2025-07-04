69db892f390a11e15369770c61c741b36ccac1c9
Naveen910 <naveenchary9550@gmail.com>
2025-07-04 17:57:00 +0530

    new file:   geo-survey-platform/src/assets/Construction.png 	new file:   geo-survey-platform/src/assets/Property.png 	new file:   geo-survey-platform/src/assets/SiteInvestigation.png 	new file:   geo-survey-platform/src/components/SystemOverview.jsx 	modified:   geo-survey-platform/src/pages/Dashboard.jsx 	new file:   geo-survey-platform/src/styles/systemoverview.css

--- geo-survey-platform/src/pages/Dashboard.jsx
+++ geo-survey-platform/src/pages/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
 import QuickActions from '../components/QuickActions';
+import SystemOverview from '../components/SystemOverview';
 
 const Dashboard = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <Header />
       <QuickActions />
+      <SystemOverview />
 
       
       <main className="p-6">

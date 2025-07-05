059cf7e758a544efbe52fecd62be51bb857f45f2
Naveen910 <naveenchary9550@gmail.com>
2025-07-05 12:34:12 +0530

    modified:   geo-survey-platform/src/pages/Dashboard.jsx

--- geo-survey-platform/src/pages/Dashboard.jsx
+++ geo-survey-platform/src/pages/Dashboard.jsx
@@ -3,12 +3,10 @@ import '../styles/dashboard.css';
 import Header from '../components/Header';
 import QuickActions from '../components/QuickActions';
 import SystemOverview from '../components/SystemOverview';
-<<<<<<< HEAD
 import ContentSection from '../components/ContentSection';
 import QuickLinks from '../components/QuickLinks';
 import { useNavigate } from 'react-router-dom';
-=======
->>>>>>> 1bd346e7f9bc7794b9af12590297d4890fc85e3e
+
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -26,18 +24,17 @@ const Dashboard = () => {
       <Header />
       <QuickActions />
       <SystemOverview />
-<<<<<<< HEAD
+
       <div className="main-content-wrapper">
         <ContentSection />
         <QuickLinks onQuickLinkClick={handleQuickLinkClick} />
       </div>
-=======
+
 
       
       <main className="p-6">
         
       </main>
->>>>>>> 1bd346e7f9bc7794b9af12590297d4890fc85e3e
     </div>
   );
 };

4972b38af72106c99165ead6d1bab7687b179a01
Naveen910 <naveenchary9550@gmail.com>
2025-07-30 21:34:51 +0530

    modified:   .gitignore

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,27 @@ const app = express();
 app.use(cors()); 
 
 const geoConfig = {
-  geoserverUrl: 'http://65.1.101.129:8080/geoserver/wms',
-  layers: [
-    {
-      name: 'topp:states',
-      workspace: 'topp',
-      layer: 'states',
-    },
-  ],
+  geoserverUrl: 'http://65.1.101.129:8080/geoserver',
+  wms: {
+    endpoint: '/wms',
+    layers: [
+      {
+        name: 'topp:states',
+        workspace: 'topp',
+        layer: 'states',
+      },
+    ],
+  },
+  wfs: {
+    endpoint: '/wfs',
+    featureTypes: [
+      {
+        name: 'topp:states',
+        workspace: 'topp',
+        typeName: 'states',
+      },
+    ],
+  }
 };
 
 app.get('/api/geoserver-config', (req, res) => {

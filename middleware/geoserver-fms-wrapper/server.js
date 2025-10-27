c000ada11bc30364f2f6d56bf5596ab4fb1864cb
Naveen910 <naveenchary9550@gmail.com>
2025-10-27 09:53:09 +0530

    modified:   middleware/geoserver-fms-wrapper/server.js

--- middleware/geoserver-fms-wrapper/server.js
+++ middleware/geoserver-fms-wrapper/server.js
@@ -13,8 +13,8 @@ const server = http.createServer(app);
 
 // Enable CORS and JSON parsing
 app.use(cors());
-app.use(express.json({ limit: '30mb' }));
-app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 
 // WebSocket setup
